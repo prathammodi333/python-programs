8ba004ba604064d34424b077be5e5cd92062b9d5
prathammodi333 <74258128+prathammodi333@users.noreply.github.com>
2022-04-18 21:12:30 +0530

    Assignment3

---
+++ pr3.py
@@ -0,0 +1,17 @@
+def maxArea(A, Len) :
+	area = 0
+	for i in range(Len) :
+		for j in range(i + 1, Len) :
+		
+			# Calculating the max area
+			area = max(area, min(A[j], A[i]) * (j - i))
+	return area
+
+a = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+b = [1, 1]
+
+len1 = len(a)
+print(maxArea(a, len1))
+
+len2 = len(b)
+print(maxArea(b, len2))
